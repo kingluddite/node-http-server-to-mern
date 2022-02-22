2e20c2261f89c7e911a10d2f59ef443821b9a72a
philip howley <howley.phil@gmail.com>
2022-02-21 22:51:47 -0500

    get single task

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -23,8 +23,27 @@ const createTask = async (req, res) => {
   // if you don't put task inside an object
 }
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id })
+const getTask = async (req, res) => {
+  try {
+    // destructure and use alias for improve code readability
+    const { id: taskId } = req.params
+    const task = await Task.findOne({ _id: taskId })
+    // check if we found a task with that id
+    if (!task) {
+      // we didn't find a task with that id :(
+      // MAKE SURE TO ALWAYS USE A RETURN HERE
+      // if not you will send more than one response which will cause an ERROR
+      // the `return` tells JavaScript you want to return from the function
+      // resource not found is 404
+      return res.status(404).json({ msg: `No task with id : ${taskId}` })
+    }
+
+    // we get here all is well and 200 means success
+    res.status(200).json(task)
+  } catch (error) {
+    // we get here and we have a server error :(
+    res.status(500).json({ msg: error })
+  }
 }
 
 const updateTask = (req, res) => {
