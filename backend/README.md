6c8503fc575ed2a253eb074eb9ffef1790d25a66
philip howley <howley.phil@gmail.com>
2022-02-25 23:20:26 -0500

    add custom asyncWrapper and notes

--- backend/README.md
+++ backend/README.md
@@ -1,63 +1,74 @@
-# Route Not Found 404
+# Add Async Wrappers for all our controllers
 
-## View our app in browser
-`http://localhost:3000/`
+## Why?
+* All the try/catch blocks are redundant
+* Let's make our code more DRY
 
+## There should be a better solution
+* There is and it is to set up a middleware function that will wrap our controllers
 
-## View API list of tasks
-`http://localhost:3000/api/v1/tasks`
+## There are npm 3rd party packages that do this
+* We will hand code this
 
-## If you go to route not defined like:
-`http://localhost:3000/api/v1/yo`
-
-* You get this:
-
-`Cannot GET /api/v1/yo`
-
-* Check the network tab and you will see a status of 404
-* This is the default 404
-
-## Let's set up a custom 404
-* Here is algolia's custom error - https://hn.algolia.com/api/v1/item
-
-* **IMPORTANT** The location of the custom 404 is IMPORTANT
-
-* We could add the 404 in the server.js file but we will add it inside a `middlewares` folder
-
-`middlewares/not-found.js`
+`middlewares/async.js`
 
 ```
-const notFound = (req, res) => res.status(404).send('Route does not exist')
+// call it fn or callback or cb - your choice
+// I'm doing this to avoid having to write a bunch of try/catch blocks but still have their functionality
+// to do this we set up the try/catch blocks inside of the wrapper
+const asyncWrapper = (fn) => {
+  // the first thing we want to do from this function is return another one
+  // remember in express by default right way we got access to request(req),response(res),and next
+  // And since we wrap our controller in my middleware
+  // and you will see below we are actually invoking the async() wrapper right away and now we want to pass in req, res, next (so we can access it inside our function and then down to our controller)
+  // we are using await inside the function body so that is why we set it up as async
+  // VERY IMPORTANT TO REMEMBER SO I WILL REPEAT MYSELF - below req,res,next we'll have access to right away because at the end of the day we return another function from asyncWrapper and then inside of the function body we pass req, res and next from Express down to this function body
+  // and then in the function body we'll decide what to do with our parameter with our argument which is our current controller
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
 
-module.exports = notFound
+module.exports = asyncWrapper
+
 ```
 
-### middleware or middlewares?
-* **note** The noun middleware can be countable or uncountable. In more general, commonly used, contexts, the plural form will also be middleware. However, in more specific contexts, the plural form can also be middlewares e.g. in reference to various types of middlewares or a collection of middlewares
-
-## Make sure to require our new middleware
-`server.js`
+* Now we convert our controller from this:
 
 ```
-// MORE CODE
-
-const notFound = require('./middlewares/not-found')
-// middleware
-app.use(express.static('./public'))
-
- // MORE CODE
-
-app.use('/api/v1/tasks', tasks)
-app.use(notFound)
-
-// MORE CODE
+const getAllTasks = asyncWrapper(async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    // we send back the object
+    // inside the object there is a property by the name of "tasks"
+    // res.status(200).json({ tasks: tasks })
+    res.status(200).json({ tasks })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+})
 ```
 
-## Now visit the error not defined again
-`http://localhost:3000/api/v1/yo`
+* To this:
 
-* And you will see this:
+```
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({})
+  res.status(200).json({ tasks })
+})
+```
 
-`Route does not exist`
+* We did not deal with the errors yet but let's walk through what just happened:
 
-* Try it in Postman and you will see the same output
+### Recap
+* We are waiting to find all the tasks and if we are successful we send back those tasks
+* As far as our asyncWrapper - we take our controller as an argument `fn` and since we return a function we have access to req, res, next that are coming from Express, then we set up the try/catch block and in here we use `await` because our controller is still `async` and we know that by default an `async` function will ALWAYS return a Promise - so we are awaiting for the promise to be settles (either resolved or rejected) and since we have access to req, res, next I pass it down to my controller and then if there is an error, (whether in getAllTasks or any controller) then we catch it and pass it to the next() function (we haven't defined this next set of middleware yet)
+
+### Now just apply the same asyncWrapper to all the other controllers
+* Make sure to test routes in Insomnia to make sure it works the same as before the changes
+* I created a task and it worked
+* I created a task with an empty string and I received an error (but we are working on errors next)
--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -1,117 +1,80 @@
 const Task = require('../models/Task')
-const getAllTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find({})
-    // we send back the object
-    // inside the object there is a property by the name of "tasks"
-    // res.status(200).json({ tasks: tasks })
-    res.status(200).json({ tasks })
-  } catch (error) {
-    res.status(500).json({ msg: error })
-  }
-}
+const asyncWrapper = require('../middlewares/async')
+
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({})
+  res.status(200).json({ tasks })
+})
 
 // we are communicating with Database
 // we use async / await
-const createTask = async (req, res) => {
-  try {
-    const task = await Task.create(req.body)
-    res.status(201).json({ task })
-  } catch (error) {
-    res.status(500).json({ msg: error })
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body)
+  res.status(201).json({ task })
+})
+
+const getTask = asyncWrapper(async (req, res) => {
+  // destructure and use alias for improve code readability
+  const { id: taskId } = req.params
+  const task = await Task.findOne({ _id: taskId })
+  // check if we found a task with that id
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id : ${taskId}` })
   }
-  // if you don't put task inside an object
-}
 
-const getTask = async (req, res) => {
-  try {
-    // destructure and use alias for improve code readability
-    const { id: taskId } = req.params
-    const task = await Task.findOne({ _id: taskId })
-    // check if we found a task with that id
-    if (!task) {
-      // we didn't find a task with that id :(
-      // MAKE SURE TO ALWAYS USE A RETURN HERE
-      // if not you will send more than one response which will cause an ERROR
-      // the `return` tells JavaScript you want to return from the function
-      // resource not found is 404
-      return res.status(404).json({ msg: `No task with id : ${taskId}` })
-    }
+  // we get here all is well and 200 means success
+  res.status(200).json(task)
+})
 
-    // we get here all is well and 200 means success
-    res.status(200).json(task)
-  } catch (error) {
-    // we get here and we have a server error :(
-    res.status(500).json({ msg: error })
+const deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskId } = req.params
+  const task = await Task.findOneAndDelete({ _id: taskId })
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id : ${taskId}` })
   }
-}
+  res.status(200).json({ task })
+  // res.status(200).send()
+  // res.status(200).json({ task: null, status: 'success' })
+})
 
-const deleteTask = async (req, res) => {
-  try {
-    const { id: taskId } = req.params
-    const task = await Task.findOneAndDelete({ _id: taskId })
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id : ${taskId}` })
-    }
-    res.status(200).json({ task })
-    // res.status(200).send()
-    // res.status(200).json({ task: null, status: 'success' })
-  } catch (error) {
-    res.status(500).json({ msg: error })
+const updateTask = asyncWrapper(async (req, res) => {
+  // grab the id parameter from the URL
+  const { id: taskId } = req.params
+
+  // search for task
+  const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+    new: true, // will return the new item that was updated
+    runValidators: true, // no explanation needed as this is obvious as to what it does
+  })
+
+  // check if task id exists
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id : ${taskId}` })
   }
-}
 
-const updateTask = async (req, res) => {
-  try {
-    // grab the id parameter from the URL
-    const { id: taskId } = req.params
+  // pass in the data we are updating from the request body
+  res.status(200).json({ task })
+})
 
-    // search for task
-    const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
-      new: true, // will return the new item that was updated
-      runValidators: true, // no explanation needed as this is obvious as to what it does
-    })
+const editTask = asyncWrapper(async (req, res) => {
+  // grab the id parameter from the URL
+  const { id: taskId } = req.params
 
-    // check if task id exists
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id : ${taskId}` })
-    }
+  // search for task
+  const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+    new: true, // will return the new item that was updated
+    runValidators: true, // no explanation needed as this is obvious as to what it does
+    overwrite: true, // tell Mongoose to overwrite the properties (because we are using the PUT method)
+  })
 
-    // pass in the data we are updating from the request body
-    res.status(200).json({ task })
-  } catch (error) {
-    res.status(500).json({ msg: error })
+  // check if task id exists
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id : ${taskId}` })
   }
-}
 
-// here we review adding a PUT request
-// difference between PUT and PATCH Method
-// PUT (explanation)
-// if I send only one property in the req.body my expectaction is that all of the other properties will be removed from that item
-// we just have to make one change in our controller but mongoose by default does not do that and we need to tell it to overwrite the resource in the options like the following:
-const editTask = async (req, res) => {
-  try {
-    // grab the id parameter from the URL
-    const { id: taskId } = req.params
-
-    // search for task
-    const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
-      new: true, // will return the new item that was updated
-      runValidators: true, // no explanation needed as this is obvious as to what it does
-      overwrite: true, // tell Mongoose to overwrite the properties (because we are using the PUT method)
-    })
-
-    // check if task id exists
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id : ${taskId}` })
-    }
-
-    // pass in the data we are updating from the request body
-    res.status(200).json({ task })
-  } catch (error) {
-    res.status(500).json({ msg: error })
-  }
-}
+  // pass in the data we are updating from the request body
+  res.status(200).json({ task })
+})
 
 module.exports = {
   getAllTasks,
