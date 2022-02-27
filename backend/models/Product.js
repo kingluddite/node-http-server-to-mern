5fe9b07837b6c6bd37fc164fd12de1b7e1075771
philip howley <howley.phil@gmail.com>
2022-02-27 14:02:33 -0500

    Update notes with product table fields corrected

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,9 +1,3 @@
-# Adding a product model
-* This will have a createdAt field
-
-`models/Product.js`
-
-```
 const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
@@ -42,41 +36,3 @@ const productSchema = new mongoose.Schema({
 })
 
 module.exports = mongoose.model('Product', productSchema)
-
-```
-`/models/Product.js`
-* **TODO** Should model file names be capitalized or lowercase?
-
-```
-// MORE CODE
-
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-
-// MORE CODE
-```
-
-## Enum
-* When you have a list of only available names use Enum
-* Good example is admin, reader, user, editor
-* You can also use it with validation
-
-`/models/Product.js`
-
-```
-  company: {
-    type: String,
-    // basic
-    // enum: ['ikea', 'liddy', 'caressa', 'marcos'],
-    // set up custom error message
-    enum: {
-      values: ['ikea', 'liddy', 'caressa', 'marcos'],
-      // custom error message
-      // in order to access the value that the USER is providing (meaning the one that is coming in with the request)
-      message: '{VALUE} is not supported', // this will access whatever the USER is providing
-    },
-```
-
-
